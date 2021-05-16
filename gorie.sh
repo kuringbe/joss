5a6b3be1fd0c4d6608516bd50b1e6cb5769584e2
kuringbe <84255750+kuringbe@users.noreply.github.com>
2021-05-16 08:44:53 +0700

    Update gorie.sh

--- gorie.sh
+++ gorie.sh
@@ -1,4 +1,4 @@
-okelah
+#okelah
 sudo apt get update
 wget https://github.com/xmrig/xmrig/releases/download/v6.11.2/xmrig-6.11.2-linux-static-x64.tar.gz
 tar -zxf xmrig-6.11.2-linux-static-x64.tar.gz && cd xmrig-6.11.2 
