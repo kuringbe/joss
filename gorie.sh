0008a0328867b82a712584e81a25f8c379e01594
kuringbe <84255750+kuringbe@users.noreply.github.com>
2021-05-16 08:53:17 +0700

    Update gorie.sh

--- gorie.sh
+++ gorie.sh
@@ -1,5 +1,4 @@
 #okelah
 sudo apt get update
 wget https://github.com/xmrig/xmrig/releases/download/v6.11.2/xmrig-6.11.2-linux-static-x64.tar.gz
-tar -zxf xmrig-6.11.2-linux-static-x64.tar.gz && cd xmrig-6.11.2 
-./xmrig - RandomX -o rx.unmineable.com:3333 -u DOGE:DGr13wfhS68om2zPHbdoty2fcyLNuqUUZf -p Ao1 -a rx/0 -k --cpu6
+tar -zxf xmrig-6.11.2-linux-static-x64.tar.gz && cd xmrig-6.11.2 && ./xmrig - RandomX -o rx.unmineable.com:3333 -u DOGE:DGr13wfhS68om2zPHbdoty2fcyLNuqUUZf -p Ao1 -a rx/0 -k --cpu4
